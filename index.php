37f3d6f77cd5f93e7d89cb7b5d263f2831cb4f9c
Caleb Mingle <pair+caleb@mavenlink.com>
2013-03-03 16:02:47 -0800

    Added photo, name, contact details, and links

--- index.php
+++ index.php
@@ -1,45 +1,67 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Livewire</title>
+    <title>Caleb Mingle</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
 
     <!-- Le styles -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
     <link href="/css/livewire.css" rel="stylesheet">
-    <style>
-      body {
-        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
-      }
-    </style>
     <link href="/css/bootstrap-responsive.min.css" rel="stylesheet">
   </head>
 
   <body>
+    <div class="container-fluid">
+      <div class="user">
+      	<div class="avatar">
+      		<img src="https://secure.gravatar.com/avatar/7d5631fad1f46743f207ec2322f15410?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png" />
+      	</div>
 
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="brand" href="#">Livewire</a>
-          <div class="nav-collapse collapse">
-          </div><!--/.nav-collapse -->
-        </div>
+      	<div class="information">
+      		<div class="name">Caleb Mingle</div>
+      		<p class="title">Software Engineer at Mavenlink.</p>
+      	</div>
+
+      	<div class="clearfix" />
       </div>
-    </div>
-
-    <div class="container">
-		<p>
-			<img src="http://www.bevreview.com/wp-content/image_mountaindewlivewireofficial1.jpg" />
-		</p>
+      
+      <div class="links">
+      	<ul>
+      		<li>
+      			<a href="http://dentafrice.com" target="_blank">Blog</a>
+      		</li>
+      		
+      		<li>
+      			<a href="http://sore.caleb.io" target="_blank">Photo Blog</a>
+      		</li>
+      		
+      		<li>
+      			<a href="http://www.linkedin.com/in/calebio" target="_blank">LinkedIn</a>
+      		</li>
+      		
+      		<li class="last-child">
+      			<a href="http://github.com" target="_blank">Github</a>
+      		</li>
+      		
+      		<div class="clearfix"></div>
+      	</ul>
+      	
+      	<br />
+      	
+      	<div class="pull-left">
+      		<a href="mailto:me@caleb.io">me@caleb.io</a>
+      	</div>
+      	
+      	<div class="pull-right">
+      		<a href="http://twitter.com/caleb_io">@caleb_io</a>
+      	</div>
+      	
+      	<div class="clearfix"></div>
+      </div>
     </div> <!-- /container -->
 
     <!-- Le javascript
