eeba80e564214a1ea94943ad9a766ee113854352
Caleb Mingle <pair+caleb@mavenlink.com>
2013-03-03 16:28:46 -0800

    Added tracking

--- index.php
+++ index.php
@@ -69,5 +69,7 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
+    
+    <?php include "_inc/tracking.php"; ?>
   </body>
 </html>
