bb12d668b96114a605b231a1b267fb7928f6a51f
Caleb Mingle <pair+caleb@mavenlink.com>
2013-03-03 19:30:58 -0800

    Updated design

--- index.php
+++ index.php
@@ -17,40 +17,21 @@
   <body>
     <div class="container-fluid">
       <div class="user">
-      	<div class="avatar">
-      		<img src="https://secure.gravatar.com/avatar/7d5631fad1f46743f207ec2322f15410?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png" />
-      	</div>
-
       	<div class="information">
       		<div class="name">Caleb Mingle</div>
-      		<p class="title">Software Engineer at Mavenlink.</p>
+      		<p class="title">Software Engineer at Mavenlink</p>
       	</div>
 
       	<div class="clearfix" />
       </div>
       
       <div class="links">
-      	<ul>
-      		<li>
-      			<a href="http://dentafrice.com" target="_blank">Blog</a>
-      		</li>
-      		
-      		<li>
-      			<a href="http://sore.caleb.io" target="_blank">Photo Blog</a>
-      		</li>
-      		
-      		<li>
-      			<a href="http://www.linkedin.com/in/calebio" target="_blank">LinkedIn</a>
-      		</li>
-      		
-      		<li class="last-child">
-      			<a href="http://github.com" target="_blank">Github</a>
-      		</li>
-      		
-      		<div class="clearfix"></div>
+      	<ul class="nav nav-pills">
+        	<li><a href="http://dentafrice.com" target="_blank">Blog</a></li>
+        	<li><a href="http://sore.caleb.io" target="_blank">Photo Blog</a></li>
+        	<li><a href="http://linkedin.com/in/calebio" target="_blank">LinkedIn</a></li>
+        	<li><a href="http://github.com" target="_blank">Github</a></li>
       	</ul>
-      	
-      	<br />
       	
       	<div class="pull-left">
       		<a href="mailto:me@caleb.io">me@caleb.io</a>
